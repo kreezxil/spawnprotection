2001053dc71d5e8b656670e56b6b8f49c2bba750
Kreezxil <kreezxil@gmail.com>
2018-02-15 17:03:43 -0600

    Oops!
    Accidental exposure of op level commands to non-ops now fixed!

--- src/main/java/com/kreezcraft/spawnprotection/commands/CommandDimension.java
+++ src/main/java/com/kreezcraft/spawnprotection/commands/CommandDimension.java
@@ -106,7 +106,10 @@ public class CommandDimension extends CommandBase {
 
 	@Override
 	public boolean checkPermission(MinecraftServer server, ICommandSender sender) {
-		return true;
+		if(server.getPlayerList().getOppedPlayers().getGameProfileFromName(sender.getName()) != null) {
+			return true; //ops can use the command
+		}
+		return false;
 	}
 
 }
